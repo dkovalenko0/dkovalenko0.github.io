dc1e5f17d3ee57988f0d44b668e12099d465de40
dkovalenko0 <dimakovalenko715@gmail.com>
2020-06-26 13:30:15 +0300

    create adaptive

--- Travel/js/main.js
+++ Travel/js/main.js
@@ -1,19 +1,18 @@
 $(function () {
-
-  $('.photos__slider').slick({
+  $(".photos__slider").slick({
     infinity: true,
     fade: true,
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
-    asNavFor: '.slider-num',
-  })
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    asNavFor: ".slider-num",
+  });
 
-  $('.slider-num').slick({
+  $(".slider-num").slick({
     fade: true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    asNavFor: '.photos__slider',
+    asNavFor: ".photos__slider",
   });
-
-
 });
