ac11d9ad65347f877fc64c3fb63a134995c092d0
dkovalenko0 <dimakovalenko715@gmail.com>
2020-07-02 19:36:34 +0300

    create adaptive

--- Organic-Food/js/main.js
+++ Organic-Food/js/main.js
@@ -1,11 +1,12 @@
 $(function () {
-
-  $('.feedback__slider ').slick({
+  $(".feedback__slider ").slick({
     dots: true,
     dotsClass: "my-dots",
     arrows: false,
+  });
 
-  })
-
-
+  $(".nav-toogler").click(function (event) {
+    $(".nav-toogler, .header__list").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 });
